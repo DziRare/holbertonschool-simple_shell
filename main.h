5e4bdf61b5003ceb63a4c2799d5bad1454771a52
Yasmin So <yasmin@yasmin.so>
2026-08-25 16:55:43 +1000

    fix: compile error

--- main.h
+++ main.h
@@ -4,5 +4,7 @@
 char **split_string(int size, char *string);
 char *read_line(void);
 int execute_command(char *command);
+int _which(char *filname, ...);
+int fork(void);
 
 #endif /* _SIMPLE_SHELL_H_ */
