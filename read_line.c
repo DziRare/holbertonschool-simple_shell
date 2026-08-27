e959e9f5f142fc8a830e94d013b0577ae618a1b1
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-27 16:29:38 +1000

    fix: Remove trailing whitespace in front of commands

--- read_line.c
+++ read_line.c
@@ -5,6 +5,22 @@
 #include "main.h"
 
 /**
+ * trim - Remove whitespace from the start of string
+ */
+void trim(char *s) {
+  
+  	// Two pointers initially at the beginning
+    int i = 0, j = 0;
+
+    // Skip leading spaces
+    while (s[i] == ' ') i++; 
+
+    // iShift the characters of string to remove
+  	// leading spaces
+    while ((s[j++] = s[i++]));
+}
+
+/**
  * read_line - Retrieves user input
  */
 char *read_line(void)
@@ -31,8 +47,10 @@ char *read_line(void)
 			buffer[read - 1] = '\0';
 			read--;
 		}
+		trim(buffer);
+
 		/* Check EOF and exit condition */
-		if (buffer == NULL || strcmp(buffer, "exit") == 0)
+		if (read == 0 || buffer == NULL || strcmp(buffer, "exit") == 0)
 		{
 			free(buffer);
 			return (NULL);
