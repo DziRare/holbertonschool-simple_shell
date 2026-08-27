deb92ad2588d365b5c9b61c2162caff001396fda
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-27 17:45:06 +1000

    feat: execute-commands-with-folders

--- exec.c
+++ exec.c
@@ -3,6 +3,39 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * split_string - splits string into array
+ * @string - string input
+ * @size - size of array
+ *
+ * Return: array of string, NULL otherwise
+ */
+char **split_string(int size, char *string)
+{
+    char **array;
+    char *token;
+    int i;
+
+    array = malloc(sizeof(string) * size);
+    if (array == NULL || size == 0)
+    {
+        return (NULL);
+    }
+
+    token = strtok(string, " ");
+    i = 0;
+    while (i < size - 1)
+    {
+        array[i] = token;
+        token = strtok(NULL, " ");
+        i = i + 1;
+    }
+	array[i] = NULL;
+
+    return array;
+}
 
 /**
  * execute_command - Run commands
@@ -13,25 +46,41 @@ int execute_command(char *command)
 {
 	pid_t child;
 	int status;
+	int i;
+	int size;
+	char **input;
+	char *env[] = {"PATH=/bin/", NULL};
 
-	char *env_args[] = {"PATH=/bin/", NULL};
-	char *argv[] = {NULL, NULL};
+	i = 0;
+	size = 1;
 
-	argv[0] = command;
+	while (command[i] != '\0')
+	{
+		if (command[i] == ' ')
+		{
+			size = size + 1;
+		}
+		i = i + 1;
+	}
+	size = size + 1;
+
+	input = split_string(size, command);
 	
 	child = fork();
 	if (child == -1)
 	{
+		free(input);
 		perror("Error");
 		return (1);
 	}
 
 	if (child == 0)
 	{
-		if (execve(argv[0], argv, env_args) == -1)
+		if (execve(input[0], input, env) == -1)
 		{
 			perror("Error");
 			free(command);
+			free(input);
 			exit(1);
 		}
 	}
